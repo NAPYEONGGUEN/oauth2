c22c4988173f63c72429a5984c9d36aa8698008e
나평근 <skvudrms54@gmail.com>
2020-08-08 14:25:23 +0900

    Update docker-compose.yml, dockerfile, and 4 more files...

--- pjt-oauth2-server/src/main/java/com/itbuddy/oauth2/config/Oauth2Config.java
+++ pjt-oauth2-server/src/main/java/com/itbuddy/oauth2/config/Oauth2Config.java
@@ -14,7 +14,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 /**
